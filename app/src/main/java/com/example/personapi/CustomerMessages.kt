acf14a1f3a2b8d14c885931801fb680743ae439a
Robin <rxs4910@g.rit.edu>
2019-05-02 16:37:01 +0200

    Minor changes to layout

--- app/src/main/java/com/example/personapi/CustomerMessages.kt
+++ app/src/main/java/com/example/personapi/CustomerMessages.kt
@@ -2,6 +2,9 @@ package com.example.personapi
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.personapi.view_models.CustomerName
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.ViewHolder
 import kotlinx.android.synthetic.main.activity_customer_messages.*
 
 class CustomerMessages : AppCompatActivity() {
@@ -9,6 +12,12 @@ class CustomerMessages : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_customer_messages)
-        
+
+        val adapter = GroupAdapter<ViewHolder>()
+        adapter.add(CustomerName())
+        adapter.add(CustomerName())
+        adapter.add(CustomerName())
+
+        customerMessageID.adapter = adapter
     }
 }
